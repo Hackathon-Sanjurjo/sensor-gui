1b5d1c01e9a24b7a6ca4dada69c1ab43db6b53bf
Miguel Sánchez de León Peque <peque@neosit.es>
2016-02-28 15:21:27 +0100

    Add label to VisSensors

--- sensorgui/vis_sensors.py
+++ sensorgui/vis_sensors.py
@@ -8,6 +8,7 @@ class VisSensors(pg.PlotWidget):
 
         self.setRange(yRange=(-100, 100))
         self.hideAxis('bottom')
+        self.setLabel('left', 'Rotation', 'deg')
         self.legend = self.addLegend(offset=(-50, 20))
         self.x_angle = self.plot(pen='y', name='x_angle')
         self.y_angle = self.plot(pen='r', name='y_angle')
