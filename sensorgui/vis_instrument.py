9014c9c22f21637fb35cd85735f8e8a1097225de
Iria Martínez <iriamtnz@gmail.com>
2016-02-28 15:35:39 +0100

    Add legend

--- sensorgui/vis_instrument.py
+++ sensorgui/vis_instrument.py
@@ -4,8 +4,12 @@ import numpy as np
 class VisInstrument(pg.PlotWidget):
 	def __init__(self):
 		super().__init__()
-		self.setRange(yRange=(0,8000))
-		self.setRange(xRange=(-0.4,0.8))
+		self.setRange(yRange=(0,1))
+		self.hideAxis('bottom')
+		self.setLabel('left', 'Acceleration', 'g')
+		self.legend = self.addLegend(offset=(-50, 20))
+		self.legend1 = self.plot(pen='y', name='x_accel')
+		self.legend2 = self.plot(pen='r', name='y_accel')
 		x_ax = np.arange(1)
 
 
@@ -14,9 +18,8 @@ class VisInstrument(pg.PlotWidget):
 		x_ax = np.arange(1)
 		bg1 = pg.BarGraphItem(x=x_ax, height=abs(x), width=0.3, brush='y')
 		bg2 = pg.BarGraphItem(x=x_ax+0.33, height=abs(y), width=0.3, brush='r')
-		#bg3 = pg.BarGraphItem(x=x_ax+0.66, height=abs(z), width=0.3, brush='g')
 		self.addItem(bg1)
 		self.addItem(bg2)
-		#self.addItem(bg3)
+
 
 
